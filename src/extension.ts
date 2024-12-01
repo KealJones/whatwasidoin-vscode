75114cd4eab6d8e62426c0954f73068a6d66fd65
Keal Jones <kealjones@gmail.com>
2024-11-30 20:50:39 -0700

    improve prompt
    Signed-off-by: Keal Jones <kealjones@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -3,81 +3,77 @@ import { GitExtension } from './api/git';
 
 const extensionId = 'wwid';
 
-const WWID_PROMPT = `You are an assistant who helps developers remember what they were doing. Your job is to evaluate the git diff the user gives you and then provide a brief summary of what the changes collectively would suggest the user was intending to do. Do not make your summary longer than 975 characters.`;
-// TODO add an example of what a response could look like: "Here is an example of what your response should look like: ..." "If you cannot figure out a single intention then give a brief summary of the most important changes."
+const WWID_PROMPT = `Evaluate the git diff provided and give a brief summary of what the changes collectively would suggest the user was intending to do. DO NOT make your summary longer than 900 characters. Here is an example of what your response should look like: "It looks like you were trying to switch from using npm to pnpm" or "It looks like you were trying to refactor and enhance the bubble game project. The changes include adding a detailed story to the README, updating the viewport settings in the HTML, adding new dependencies and removing some in \`package.json\`, and refactoring the main application code to use ECS (Entity Component System) for managing bubbles. You also updated the CSS for better styling and animations, and switched from Redux to Jotai for state management. Additionally, you made changes to support BigInt in JSON parsing and updated the main entry point to remove the Redux provider."`;
 
 export function activate(context: vscode.ExtensionContext) {
   const wwid = new WhatWasIDoin();
-  let result = '';
   console.log(`Congratulations, your extension ${extensionId} is now active!`);
 
   const disposable = vscode.commands.registerCommand(
     `${extensionId}.whatwasidoin`,
     async () => {
-      let diff = '';
       try {
-        vscode.window.withProgress(
+        await vscode.window.withProgress(
           {
-            location: vscode.ProgressLocation.Window,
-            cancellable: false,
+            location: vscode.ProgressLocation.Notification,
+            cancellable: true,
             title: 'Figuring out what you were doing...',
           },
-          async (progress) => {
+          async (progress, token) => {
+            token.onCancellationRequested(() => {
+              console.log('User canceled the long running operation');
+            });
+
             progress.report({ increment: 0 });
 
-            await Promise.resolve();
+            const gitDiffResult = await getGitDiff();
 
-            progress.report({ increment: 100 });
+            if (!gitDiffResult) {
+              vscode.window.showInformationMessage(
+                "The 'What was i doin' extension only works with git repos that have uncommitted changes. No changes found in the Git repository.",
+              );
+            }
+
+            progress.report({ increment: 50 });
+
+            let [model] = await vscode.lm.selectChatModels({
+              vendor: 'copilot',
+              family: 'gpt-4o',
+            });
+            const messages = [
+              vscode.LanguageModelChatMessage.User(WWID_PROMPT),
+              vscode.LanguageModelChatMessage.User(gitDiffResult!),
+            ];
+
+            // make sure the model is available
+            if (model) {
+              try {
+                let chatResponse = await model.sendRequest(
+                  messages,
+                  {},
+                  new vscode.CancellationTokenSource().token,
+                );
+                if (chatResponse) {
+                  // handle chat response
+                  const result = await parseChatResponse(chatResponse);
+                  progress.report({ increment: 100 });
+                  vscode.window.showInformationMessage(result);
+                }
+              } catch (err) {
+                if (err instanceof vscode.LanguageModelError) {
+                  vscode.window.showErrorMessage(
+                    `${err.message}, ${err.code}, ${err.cause}`,
+                  );
+                } else {
+                  throw err;
+                }
+                return;
+              }
+            }
           },
         );
-
-        const gitDiffResult = await getGitDiff();
-
-        if (gitDiffResult) {
-          [diff] = gitDiffResult;
-          console.log(diff);
-        } else {
-          vscode.window.showInformationMessage(
-            "The 'What was i doin' extension only works with git repos that have uncommitted changes. No changes found in the Git repository.",
-          );
-        }
       } catch (error) {
         vscode.window.showErrorMessage(`Error: ${error}`);
-      }
-
-      // const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-      let [model] = await vscode.lm.selectChatModels({
-        vendor: 'copilot',
-        family: 'gpt-4o',
-      });
-      const messages = [
-        vscode.LanguageModelChatMessage.User(WWID_PROMPT),
-        vscode.LanguageModelChatMessage.User(diff),
-      ];
-
-      // make sure the model is available
-      if (model) {
-        try {
-          let chatResponse = await model.sendRequest(
-            messages,
-            {},
-            new vscode.CancellationTokenSource().token,
-          );
-          if (chatResponse) {
-            // handle chat response
-            result += await parseChatResponse(chatResponse);
-            vscode.window.showInformationMessage(result);
-          }
-        } catch (err) {
-          if (err instanceof vscode.LanguageModelError) {
-            vscode.window.showErrorMessage(
-              `${err.message}, ${err.code}, ${err.cause}`,
-            );
-          } else {
-            throw err;
-          }
-          return;
-        }
       }
     },
   );
